23848489d2e61d0f6fc34acd999329f4eed509e7
nasrinna <nasrin.ahmad@noorteck.com>
2025-01-29 21:10:06 -0500

    John

--- src/Demo1/DemoTest1.java
+++ src/Demo1/DemoTest1.java
@@ -5,5 +5,10 @@ public class DemoTest1 {
 	
 	public static void main(String[] args) {
 		System.out.println("Java");
+		
+		String name = "john";
+		String lastName = "cena";
+		System.out.println("John");
+		System.out.println("cena");
 	}
 }
